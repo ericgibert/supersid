631524cf9e8406f65429a987c0b9cf51fce838a3
ericgibert <ericgibert@yahoo.fr>
2013-11-19 17:41:27 +0800

    --read param is kept as a second mem buffer
    current buffer is based on .cfg parameters, only data from --read sid file are copied over.

--- supersid/logger.py
+++ supersid/logger.py
@@ -8,50 +8,51 @@ from sidfile import SidFile
 
 class Logger():
     def __init__(self, controller, read_file):
-        self.version = "1.3.1 20130907"
+        self.version = "1.3.1 20131118"
         self.controller = controller
         self.config = controller.config
+        # first create in memory buffers
+        if len(self.config.stations) == 1:
+            # only one station to monitor, let's default to SID file format
+            self.controller.isSuperSID = False
+            self.config["stationid"] = self.config.stations[0]['call_sign']
+            self.config["frequency"] = self.config.stations[0]['frequency']
+            self.config['log_format'] = "sid_format"
+        elif len(self.config.stations) > 1:
+            # more than one station to monitor, let's default to SuperSId file format
+            self.controller.isSuperSID = True
+            self.config["stations"] = ",".join([s['call_sign'] for s in self.config.stations])
+            self.config["frequencies"] = ",".join([s['frequency'] for s in self.config.stations])
+            self.config['log_format'] = "supersid_format"
+        else:
+            print("Error: no station to log???")
+            exit(5)
+        self.sid_file = SidFile(sid_params = self.config)
+
         # Do we have a file to read?
         if read_file:
-            self.sid_file = SidFile(filename = read_file)
-            if self.sid_file.sid_params['logtype'] != 'raw':
-                print("The file type is not raw but", self.sid_file.sid_params['logtype'])
+            sid_file2 = SidFile(filename = read_file)
+            if sid_file2.sid_params['logtype'] != 'raw':
+                print("The file type is not raw but", sid_file2.sid_params['logtype'])
                 answer = raw_input("Do you still want to keep its content and continue recording? [y/N]")
                 if answer.lower()!='y':
                     print("Abort.")
                     exit(-10)
-            elif self.sid_file.sid_params['utc_starttime'] != strftime("%Y-%m-%d 00:00:00", gmtime()):
-                print("Not today's file. The file UTC_StartTime =", self.sid_file.sid_params['utc_starttime'])
+            elif sid_file2.sid_params['utc_starttime'] != strftime("%Y-%m-%d 00:00:00", gmtime()):
+                print("Not today's file. The file UTC_StartTime =", sid_file2.sid_params['utc_starttime'])
                 answer = raw_input("Do you still want to keep its content and continue recording? [y/N]")
                 if answer.lower()!='y':
                     print("Abort.")
                     exit(-11)
-            elif sorted(self.sid_file.stations) != sorted([s['call_sign'] for s in self.config.stations]):
-                print("Station Lists are different:", self.sid_file.stations, "!=", [s['call_sign'] for s in self.config.stations])
+            elif sorted(sid_file2.stations) != sorted([s['call_sign'] for s in self.config.stations]):
+                print("Station Lists are different:", sid_file2.stations, "!=", [s['call_sign'] for s in self.config.stations])
                 answer = raw_input("Do you still want to keep its content and continue recording? [y/N]")
                 if answer.lower()!='y':
                     print("Abort.")
                     exit(-11)
-            print("Continue recoding with data from file", read_file, "included.")
-        else:
-            # no file to continue: fresh buffer in memory
-            if len(self.config.stations) == 1:
-                # only one station to monitor, let's default to SID file format
-                self.controller.isSuperSID = False
-                self.config["stationid"] = self.config.stations[0]['call_sign']
-                self.config["frequency"] = self.config.stations[0]['frequency']
-                self.config['log_format'] = "sid_format"
-            elif len(self.config.stations) > 1:
-                # more than one station to monitor, let's default to SuperSId file format
-                self.controller.isSuperSID = True
-                self.config["stations"] = ",".join([s['call_sign'] for s in self.config.stations])
-                self.config["frequencies"] = ",".join([s['frequency'] for s in self.config.stations])
-                self.config['log_format'] = "supersid_format"
-            else:
-                print("Error: no station to log???")
-                exit(5)
-            self.sid_file = SidFile(sid_params = self.config)
-    
+            self.sid_file.copy_data(sid_file2)
+            print("Continue recording with data from file", read_file, "included.")
+
     def log_sid_format(self, stations,  filename='', log_type='filtered', extended = False):
         """ One file per station. By default, buffered data is filtered."""
         filenames = []
