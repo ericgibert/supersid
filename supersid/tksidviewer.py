999b474cd87976350fac3a1c51e859482dfba510
eric <eric.gibert@yahoo.fr>
2015-04-24 20:38:28 +0800

    tkinter implementation

--- supersid/tksidviewer.py
+++ supersid/tksidviewer.py
@@ -17,7 +17,7 @@ else:
 import supersid_plot as SSP
 from config import FILTERED, RAW
 
-class tkSidViewer(tk.Frame):
+class tkSidViewer():
     '''
     Create the Tkinter GUI
     '''
@@ -29,61 +29,59 @@ class tkSidViewer(tk.Frame):
         self.version = "1.3.2 20150421"
         self.controller = controller  # previously referred as 'parent'
         self.tk_root = tk.Tk()
-        tk.Frame.__init__(self, parent=None, background="white")
         self.tk_root.title("supersid @ " + self.controller.config['site_name'])
 
         # All Menus creation
 
-        # Frame
-        self.pack(fill=tk.BOTH, expand=1)
-
         # FigureCanvas
         self.psd_figure = Figure(facecolor='beige')
-        self.canvas = FigureCanvas(self.psd_figure, self)
+        self.canvas = FigureCanvas(self.psd_figure, master=self.tk_root)
         self.canvas.show()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-        self.bind('<Configure>', self.on_resize)
 
-        self.toolbar = NavigationToolbar2TkAgg( self.canvas, self )
+        self.toolbar = NavigationToolbar2TkAgg( self.canvas, self.tk_root)
         self.toolbar.update()
         self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
         self.axes = self.psd_figure.add_subplot(111)
-        self.axes.hold(True)
+        self.axes.hold(False)
 
         # StatusBar
         self.statusbar_txt = tk.StringVar()
         self.label=tk.Label(self.tk_root, bd=1, relief=tk.SUNKEN, anchor=tk.W,
                            textvariable=self.statusbar_txt,
-                           font=('arial',16,'normal'))
+                           font=('arial',12,'normal'))
         self.statusbar_txt.set('Initialization...')
         self.label.pack(fill=tk.X)
-        self.pack()
-
-        # Default View
 
     def run(self):
-        self.mainloop()
+        self.need_refresh = False
+        self.refresh_psd() # start the re-draw loop
+        self.tk_root.mainloop()
+
 
     def close(self):
-        self.quit()
-
+        self.tk_root.quit()
 
     def clear(self):
-        self.axes.cla()  # erase previous curve before drawing the new one
-
+        #self.axes.cla()  # erase previous curve before drawing the new one
+        pass
 
     def status_display(self, message, level=0, field=0):
         """update the main frame by changing the message in status bar"""
-        print(message)
+        #print(message)
         self.statusbar_txt.set(message)
 
     def get_psd(self, data, NFFT, FS):
         """By calling 'psd' within axes, it both calculates and plots the spectrum"""
         Pxx, freqs = self.axes.psd(data, NFFT = NFFT, Fs = FS)
+        self.need_refresh = True
         return Pxx, freqs
 
-    def on_resize(self, event):
-        w,h = event.width, event.height
-        #self.config(width=w, height=h)
-        print(w,h)
+    def refresh_psd(self, z=None):
+        """redraw the graphic PSD plot if needed i.e.new data have been given to get_psd"""
+        if self.need_refresh:
+            self.canvas.draw()
+            self.need_refresh = False
+        self.tk_root.after(500, self.refresh_psd)
+
